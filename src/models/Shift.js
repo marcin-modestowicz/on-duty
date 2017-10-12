dad7051649b285e44cfbc848dbd048ab63c8d4f5
Marcin Modestowicz <marcin@modestowicz.com>
2017-10-12 09:59:48 +0200

    Update Shift model to handle both full and picked users list

--- src/models/Shift.js
+++ src/models/Shift.js
@@ -2,23 +2,46 @@
 import { observable, action } from "mobx";
 import type User from "./User";
 
+const USERS_PER_SHIFT = 2;
+
 export class Shift {
-  @observable onDuty: User[];
+  _onDuty: User[];
+  sealed: boolean;
 
   constructor(users: User[] = []) {
-    this.onDuty = users;
+    this._onDuty = users;
   }
 
   @action
   addUser = (user: User) => {
-    this.onDuty.push(user);
+    this._onDuty.push(user);
   };
 
   @action
   removeUser = (id: string) => {
-    const index = this.onDuty.findIndex(user => user.id === id);
-    this.onDuty.splice(index, 1);
+    const index = this._onDuty.findIndex(user => user.id === id);
+    this._onDuty.splice(index, 1);
   };
+
+  seal() {
+    this.sealed = true;
+  }
+
+  unseal() {
+    this.sealed = false;
+  }
+
+  get isSealed(): boolean {
+    return this.sealed;
+  }
+
+  get onDuty(): User[] {
+    if (this.sealed) {
+      return this._onDuty.slice(0, USERS_PER_SHIFT);
+    } else {
+      return this._onDuty;
+    }
+  }
 }
 
 export default Shift;
