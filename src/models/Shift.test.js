13b7f9f8ea9b464f5cb9dba9d9275f1a44c44b38
Marcin Modestowicz <marcin@modestowicz.com>
2017-10-26 13:52:38 +0200

    Add Shift fill and toggleUser methods tests

--- src/models/Shift.test.js
+++ src/models/Shift.test.js
@@ -29,4 +29,42 @@ describe("Shift model", () => {
       expect(shift.onDuty.slice()).toEqual([]);
     });
   });
+
+  describe("method fill", () => {
+    test("should fill the shift up to the limit", () => {
+      const user1 = new User("Homer Simpson");
+      const user2 = new User("Marge Simpson");
+      const user3 = new User("Bart Simpson");
+
+      shift.fill([user1, user2, user3]);
+
+      expect(shift.onDuty.slice()).toEqual([user, user1]);
+    });
+  });
+
+  describe("method toggle", () => {
+    let addUserSpy;
+    let removeUserSpy;
+
+    beforeEach(() => {
+      addUserSpy = jest.spyOn(shift, "addUser");
+      removeUserSpy = jest.spyOn(shift, "removeUser");
+    });
+
+    test("should call removeUser if user is present", () => {
+      shift.toggleUser(user);
+
+      expect(addUserSpy).not.toHaveBeenCalled();
+      expect(removeUserSpy).toHaveBeenCalled();
+    });
+
+    test("should call addUser if user is not present", () => {
+      const anotherUser = new User("Homer Simpson");
+
+      shift.toggleUser(anotherUser);
+
+      expect(addUserSpy).toHaveBeenCalled();
+      expect(removeUserSpy).not.toHaveBeenCalled();
+    });
+  });
 });
