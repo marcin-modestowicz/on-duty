b335343c1c0adeb4e2eca8631058874fbeb69e94
Marcin Modestowicz <marcin@modestowicz.com>
2017-10-28 22:56:11 +0200

    Add logged in users handling to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //@flow
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import LoginStore from "./stores/LoginStore";
 import MainStore from "./stores/MainStore";
+import Login from "./components/Login";
 import AddUser from "./components/AddUser";
 import ShowUser from "./components/ShowUser";
 import Shifts from "./components/Shifts";
@@ -9,9 +11,17 @@ import styles from "./App.scss";
 
 @observer
 class App extends Component<null> {
+  loginStore: LoginStore = new LoginStore();
   mainStore: MainStore = new MainStore();
 
   render() {
+    const {
+      isLoggedIn,
+      isLoggingIn,
+      user,
+      handleLogin,
+      handleLogout
+    } = this.loginStore;
     const {
       users,
       addUser,
@@ -22,26 +32,34 @@ class App extends Component<null> {
       summary
     } = this.mainStore;
 
+    if (isLoggingIn) {
+      return null;
+    }
+
     return (
       <div className={styles.root}>
         <div className={styles.header}>
           <h2>On Duty</h2>
         </div>
-        <div>
+        {!isLoggedIn && <Login onLogin={handleLogin} />}
+        {isLoggedIn && (
           <div>
-            <AddUser onAdd={addUser} />
-            <button onClick={saveState}>Save state</button>
-            <button onClick={fillCalendar} disabled={!isReady}>
-              Create calendar
-            </button>
-            {users.map(user => <ShowUser key={user.id} user={user} />)}
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+              <AddUser onAdd={addUser} />
+              <button onClick={saveState}>Save state</button>
+              <button onClick={fillCalendar} disabled={!isReady}>
+                Create calendar
+              </button>
+              {users.map(user => <ShowUser key={user.id} user={user} />)}
+            </div>
+            <div>
+              {calendar && (
+                <Shifts calendar={calendar} summary={summary} users={users} />
+              )}
+            </div>
           </div>
-          <div>
-            {calendar && (
-              <Shifts calendar={calendar} summary={summary} users={users} />
-            )}
-          </div>
-        </div>
+        )}
       </div>
     );
   }
